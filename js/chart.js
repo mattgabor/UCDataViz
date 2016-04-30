14a09e1564e3a21caa24adde3d9313c11982465f
Matt Gabor <mattgabor@gmail.com>
2016-04-30 16:43:56 -0700

    Added half pie

--- js/chart.js
+++ js/chart.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
                 csv: csv
             },
             title: {
-                text: 'Matt Gabor Engagement Index'
+                text: 'My Data Arts Engagement Index'
             },
             yAxis: {
                 title: {
@@ -19,37 +19,80 @@ $(document).ready(function() {
             },
             plotOptions: {
                 series: {
-                    linewidth: 2,
-                    color: '#A3EDBA'
+                    linewidth: 4,
+                    color: '#aaeeee'
+                },
+                pie: {
+                    startAngle: -90,
+                    endAngle: 90,
                 }
-            }
+            },
+            series: [{
+            }, {
+                type: 'pie',
+                name: '# of Occurrences',
+                data: [
+                {
+                    name: 'Assignment Submitted',
+                    y: 11,
+                    color: Highcharts.getOptions().colors[0]
+                },{
+                    name: 'Asset Added',
+                    y: 9,
+                    color: Highcharts.getOptions().colors[1]
+                },
+                {
+                    name: 'Like Given',
+                    y: 472,
+                    color: Highcharts.getOptions().colors[2]
+                },
+                {
+                    name: 'Like Received',
+                    y: 675,
+                    color: Highcharts.getOptions().colors[3]
+                }, {
+                    name: 'Comment Given',
+                    y: 182,
+                    color: Highcharts.getOptions().colors[4]
+                },
+                {
+                    name: 'Comment Received',
+                    y: 159,
+                    color: Highcharts.getOptions().colors[5]
+                },
+                {
+                    name: 'Reply Received',
+                    y: 43,
+                    color: Highcharts.getOptions().colors[6]
+                },
+                {
+                    name: 'Discussion Comment Given',
+                    y: 5,
+                    color: Highcharts.getOptions().colors[7]
+                },
+                {
+                    name: 'Discussion Reply Received',
+                    y: 2,
+                    color: Highcharts.getOptions().colors[8]
+                }],
+                // plotOptions: {
+                //     pie: {
+                //         startAngle: -90,
+                //         endAngle: 90,
+                //         center: ['50%', '75%']
+                //     }
+                // },
+                center: [280, 160],
+                size: 250,
+                showInLegend: false,
+                dataLabels: {
+                    enabled: true,
+                    color: '#E0E0E3',
+                    style: {
+                        "fontSize": "13px"
+                    }
+                }
+            }]
         });
     });
 });
-
-
-// $(function () {
-//     $('#container').highcharts({
-//         chart: {
-//             type: 'bar'
-//         },
-//         title: {
-//             text: 'Fruit Consumption'
-//         },
-//         xAxis: {
-//             categories: ['Apples', 'Bananas', 'Oranges']
-//         },
-//         yAxis: {
-//             title: {
-//                 text: 'Fruit eaten'
-//             }
-//         },
-//         series: [{
-//             name: 'Jane',
-//             data: [1, 0, 4]
-//         }, {
-//             name: 'John',
-//             data: [5, 7, 3]
-//         }]
-//     });
-// });
