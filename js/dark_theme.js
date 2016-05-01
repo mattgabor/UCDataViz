d188dbda8b8c41bd61edc7ef514e6f13659745dd
Matt Gabor <mattgabor@gmail.com>
2016-04-30 23:50:20 -0700

    added background color

--- js/dark_theme.js
+++ js/dark_theme.js
@@ -15,13 +15,15 @@ Highcharts.theme = {
    colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
       "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
    chart: {
-      backgroundColor: {
-         linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-         stops: [
-            [0, '#2a2a2b'],
-            [1, '#3e3e40']
-         ]
-      },
+      backgroundColor: '#3B3B3D',
+      //
+      // backgroundColor: {
+      //    linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+      //    stops: [
+      //       [0, '#2a2a2b'],
+      //       [1, '#3e3e40']
+      //    ]
+      // },
       style: {
          fontFamily: "'Lato', sans-serif"
       },
